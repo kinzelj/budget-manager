ca8ab5018fee075e3d6a314448b9c7f10db5fd17
Josh Kinzel <kinzelj008@gmail.com>
2020-01-08 16:56:26 -0600

    update home page message

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -81,7 +81,7 @@ const useStyles = makeStyles(theme => ({
 
 const getNavOptions = () => ({
   options: [
-  	{ text: "Home", icon: "large home icon"},
+  	{ text: "Home (Under Development)", icon: "large home icon"},
   	{ text: "View Transaction Data", icon: "large database icon"},
   	{ text: "Analysis", icon: "large table icon"},
   	{ text: "Import Transaction Data", icon: "large upload icon"},
