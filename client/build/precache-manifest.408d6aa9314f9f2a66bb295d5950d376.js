90c2edf2cb2ac7089f07fd1371761acba9e96afe
kinzelj <kinzelj008@gmail.com>
2020-01-15 20:47:38 -0600

    add build

---
+++ client/build/precache-manifest.408d6aa9314f9f2a66bb295d5950d376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85d04beb8f80cc4612b1daa3110e6ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8cb2ff8b4fe67f4ba1e",
+    "url": "/static/css/main.7bf9c197.chunk.css"
+  },
+  {
+    "revision": "40817b879b08c4c72d8c",
+    "url": "/static/js/2.ec0eb351.chunk.js"
+  },
+  {
+    "revision": "2a4d3813bdbfda79df1f9834c55ea045",
+    "url": "/static/js/2.ec0eb351.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8cb2ff8b4fe67f4ba1e",
+    "url": "/static/js/main.30f8cfe4.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
